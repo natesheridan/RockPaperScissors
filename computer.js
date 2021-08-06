e263f8481dd3e990e377813a3005c292b6c6018a
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-08-05 20:11:00 -0600

    Initial Commit

---
+++ computer.js
@@ -0,0 +1,25 @@
+class Computer{
+    constructor(name, token){
+        this.type = "cpu"
+        this.name = name;
+        this.wins = 0;
+        this.token = "💻";
+    }
+    takeTurn(){
+        var selection = '';
+        var randomNumber0to2 = (Math.floor(Math.random() * 3))
+        if (randomNumber0to2===0){
+            selection = "rock";
+        }
+        else if(randomNumber0to2===1){
+            selection = "paper";
+        }
+        else if(randomNumber0to2===2){
+            selection = "scissors";
+        }
+        return selection;
+    }
+
+
+}
+
