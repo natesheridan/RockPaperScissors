f780772cd56825fd5b2f50ed76151014a1c5643f
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-08-07 14:32:02 -0600

    Add selection for player

---
+++ player.js
@@ -0,0 +1,23 @@
+class Player{
+    constructor(name, token){
+        this.type = "human"
+        this.name = name;
+        this.wins = 0;
+        this.token = token;
+
+    }
+    saveWinsToStorage(){
+
+    }
+    retrieveWinsFromStorage(){
+
+    }
+    takeTurn(selection){
+        
+        return `${selection}`
+    }
+
+
+}
+
+
