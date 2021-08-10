e0d9038ca5d87444e7683974160f9d51bcc648fa
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-08-09 20:54:42 -0600

    Add gamemode param to runGame

--- game.js
+++ game.js
@@ -5,52 +5,120 @@ class Game{
         
     }
 
-    runGame(){
-
-        var p1Selection = this.player1.takeTurn();
-        var p2Selection =  this.player2.takeTurn();
-        if (p1Selection === p2Selection){
-            console.log("DRAW!")
-            return `DRAW both chose ${p1Selection}`
+    runGame(gamemode){
+        console.log(gamemode)
+        if (gamemode === "normal"){
+            var p1Selection = this.player1.takeTurn();
+            var p2Selection =  this.player2.takeTurn(gamemode);
+            if (p1Selection === p2Selection){
+                console.log("DRAW!")
+                return `DRAW both chose ${p1Selection}`
+            }
+            if (p1Selection === "rock"){
+                if(p2Selection ==="paper"){
+                    currentP2.wins++
+                    return `${this.player2.name} ${this.player2.token} WINS w/ ${p2Selection} beating ${p1Selection}!`
+                }
+                else{
+                    currentP1.wins++
+                    return `${this.player1.name} ${this.player1.token} WINS w/ ${p1Selection} beating ${p2Selection}!`
+                }
+            }
+            
+            else if (p1Selection === "paper"){
+                if(p2Selection ==="scissors"){
+                    currentP2.wins++
+                    return `${this.player2.name} ${this.player2.token} WINS w/ ${p2Selection} beating ${p1Selection}!`
+                }
+                else{
+                    currentP1.wins++
+                    return `${this.player1.name} ${this.player1.token} WINS w/ ${p1Selection} beating ${p2Selection}!`
+                }
+            }
+            
+            else if (p1Selection === "scissors"){
+                if(p2Selection ==="rock"){
+                    currentP2.wins++
+                    return `${this.player2.name} ${this.player2.token} WINS w/ ${p2Selection} beating ${p1Selection}!`
+                }
+                else{
+                    currentP1.wins++
+                    return `${this.player1.name} ${this.player1.token} WINS w/ ${p1Selection} beating ${p2Selection}!`
+                }
+            }
         }
-        else if (p1Selection === "rock"){
-            if(p2Selection ==="paper"){
-                currentP2.wins++
-                return `${this.player2.name} ${this.player2.token} WINS w/ ${p2Selection} beating ${p1Selection}!`
+        
+        if (gamemode === "spicy"){
+            var p1Selection = this.player1.takeTurn();
+            var p2Selection =  this.player2.takeTurn(gamemode);
+            if (p1Selection === p2Selection){
+                console.log("DRAW!")
+                return `DRAW both chose ${p1Selection}`
             }
-            else{
-                currentP1.wins++
-                return `${this.player1.name} ${this.player1.token} WINS w/ ${p1Selection} beating ${p2Selection}!`
+            if (p1Selection === "rock"){
+                if (p2Selection === "scissors" || p2Selection === "lizard"){
+                    currentP1.wins++
+                    return `${this.player1.name} ${this.player1.token} WINS w/ ${p1Selection} beating ${p2Selection}!`
+                }
+                else {
+                    currentP2.wins++
+                    return `${this.player2.name} ${this.player2.token} WINS w/ ${p2Selection} beating ${p1Selection}!`
+                }
+            }
+            else if (p1Selection === "paper"){
+                if (p2Selection === "rock" || p2Selection === "alien"){
+                    currentP1.wins++
+                    return `${this.player1.name} ${this.player1.token} WINS w/ ${p1Selection} beating ${p2Selection}!`
+                }
+                else {
+                    currentP2.wins++
+                    return `${this.player2.name} ${this.player2.token} WINS w/ ${p2Selection} beating ${p1Selection}!`
+                }
+            }
+            else if (p1Selection === "scissors"){
+                if (p2Selection === "paper" || p2Selection === "lizard"){
+                    currentP1.wins++
+                    return `${this.player1.name} ${this.player1.token} WINS w/ ${p1Selection} beating ${p2Selection}!`
+                }
+                else {
+                    currentP2.wins++
+                    return `${this.player2.name} ${this.player2.token} WINS w/ ${p2Selection} beating ${p1Selection}!`
+                }
+            }
+            else if (p1Selection === "lizard"){
+                if (p2Selection === "paper" || p2Selection === "alien"){
+                    currentP1.wins++
+                    return `${this.player1.name} ${this.player1.token} WINS w/ ${p1Selection} beating ${p2Selection}!`
+                }
+                else {
+                    currentP2.wins++
+                    return `${this.player2.name} ${this.player2.token} WINS w/ ${p2Selection} beating ${p1Selection}!`
+                }
+            }
+            else if (p1Selection === "alien"){
+                if (p2Selection === "scissors" || p2Selection === "rock"){
+                    currentP1.wins++
+                    return `${this.player1.name} ${this.player1.token} WINS w/ ${p1Selection} beating ${p2Selection}!`
+                }
+                else {
+                    currentP2.wins++
+                    return `${this.player2.name} ${this.player2.token} WINS w/ ${p2Selection} beating ${p1Selection}!`
+                }
             }
         }
-
-        else if (p1Selection === "paper"){
-            if(p2Selection ==="scissors"){
-                currentP2.wins++
-                return `${this.player2.name} ${this.player2.token} WINS w/ ${p2Selection} beating ${p1Selection}!`
-            }
-            else{
-                currentP1.wins++
-                return `${this.player1.name} ${this.player1.token} WINS w/ ${p1Selection} beating ${p2Selection}!`
-            }
-        }
-
-        else if (p1Selection === "scissors"){
-            if(p2Selection ==="rock"){
-                currentP2.wins++
-                return `${this.player2.name} ${this.player2.token} WINS w/ ${p2Selection} beating ${p1Selection}!`
-            }
-            else{
-                currentP1.wins++
-                return `${this.player1.name} ${this.player1.token} WINS w/ ${p1Selection} beating ${p2Selection}!`
-            }
-        }
-
     }
+}    
     
     
 
-}
+
+// ROCK > SCISSORS & LIZARD
+// SCISSORS > ROCK & ALIEN
+// PAPER > PAPER & LIZARD
+// LIZARD > PAPER & ALIEN
+// ALIEN > SCISSORS & ROCK
+
+
 
 
 // ROCK > SCISSORS
